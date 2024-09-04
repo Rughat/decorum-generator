eff7a90934ab7cc7b5d8ac36ab5eb236d029fec3
Shawn Isenhart <rughat@gmail.com>
2024-09-04 16:25:26 -0500

    Massive changes connected to reworking the games controller

--- spec/system/start_a_game_spec.rb
+++ spec/system/start_a_game_spec.rb
@@ -4,24 +4,30 @@ RSpec.describe "creating a new game" do
   context "on non-mobile" do
     it "can create a new game" do
       visit root_path
-      click "New Game"
+      click_on "New Game"
 
       expect(current_path).to eq new_game_path
 
-      fill_in "Number of players", with: "4"
-
-      click "Start Game"
+      click_on "Start a 2 player game"
 
       # expect page to be on the choose a player page
 
       expect(page).to have_content "Starting house setup"
 
-      expect(page).to have_button "Join as player 1"
-      expect(page).to have_button "Join as player 2"
-      expect(page).to have_button "Join as player 3"
-      expect(page).to have_button "Join as player 4"
+      expect(page).to have_button "Show requirements for player 1"
+      expect(page).to have_button "Show requirements for player 2"
 
-      click "Join as player 1"
+      click_on "Show requirements for player 1"
+
+      # expect(current_path).to eq player_rules_path
+
+      expect(page).to have_content "Requirements"
+
+      # expect(page).to have_content some list of requirements for player 1
+
+      click_on "Close"
+
+      click_on "Show requirements for player 2"
 
       # expect(current_path).to eq player_rules_path
 
