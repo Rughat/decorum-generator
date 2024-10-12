340ba570060ae432c573fe748b620386585da517
Shawn Isenhart <isen0011@umn.edu>
2024-10-12 13:50:02 -0500

    Extract requirements generation from player

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -4,4 +4,8 @@ class Requirement < ApplicationRecord
   def self.generate(goal:, rule_source: ComputedRule)
     rule_source.all.sample.build(house: goal)
   end
+
+  def self.generate_group(rule_count:, rule_source: ComputedRule, goal:)
+    Array.new(rule_count).map {|_| self.generate(goal: goal, rule_source: rule_source) }
+  end
 end
