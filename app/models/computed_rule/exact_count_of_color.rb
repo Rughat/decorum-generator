b37de4dc044a115646e98fe1499abea79825beaf
Shawn Isenhart <isen0011@umn.edu>
2024-10-20 09:11:21 -0400

    WIP: update exact color & spec for furnishing

--- app/models/computed_rule/exact_count_of_color.rb
+++ app/models/computed_rule/exact_count_of_color.rb
@@ -1,7 +1,7 @@
 module ComputedRule
   class ExactCountOfColor < Requirement
-    def self.build(house:, colors: Colors, sections: Section)
-      color = colors.random
+    def self.build(house:, colors: Colors, sections: Section, feature: colors.random)
+      color = feature.to_s
       section = sections.random
       count = house.count_colors(color: color, section: section)
       rule = self.create
