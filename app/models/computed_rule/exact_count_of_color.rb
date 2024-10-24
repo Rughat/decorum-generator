b3359081a031d65ad32dc004b164dab6deaf9dcd
Shawn Isenhart <isen0011@umn.edu>
2024-10-24 15:39:09 -0500

    Use generic  in rules

--- app/models/computed_rule/exact_count_of_color.rb
+++ app/models/computed_rule/exact_count_of_color.rb
@@ -1,11 +1,10 @@
 module ComputedRule
   class ExactCountOfColor < Requirement
     def self.build(house:, colors: Colors, sections: Section, feature: colors.random)
-      color = feature.to_s
       section = sections.random
-      count = house.count_colors(color: color, section: section)
+      count = house.count_colors(color: feature, section: section)
       rule = self.create
-      rule.text = "The #{section.name} must contain exactly #{count} #{color} #{"feature".pluralize(count)} (as objects and/or wall colors)"
+      rule.text = "The #{section.name} must contain exactly #{count} #{feature} #{"feature".pluralize(count)} (as objects and/or wall colors)"
       rule.save
       rule
     end
--- app/models/computed_rule/relative_count_of_style.rb
+++ app/models/computed_rule/relative_count_of_style.rb
@@ -1,15 +1,14 @@
 module ComputedRule
   class RelativeCountOfStyle < Requirement
-    def self.build(house:, styles: Style, sections: Section)
-      style = styles.random
+    def self.build(house:, styles: Style, sections: Section, feature: styles.random)
       section = sections.random_opposable
-      count = house.count_styles(style: style, section: section)
-      opposite_count = house.count_styles(style: style, section: section.opposite)
+      count = house.count_styles(style: feature, section: section)
+      opposite_count = house.count_styles(style: feature, section: section.opposite)
       rule = self.create
       rule.text = if count == opposite_count
-                    "The #{section.name} and the #{section.opposite.name} must contain an equal amount of #{style} objects"
+                    "The #{section.name} and the #{section.opposite.name} must contain an equal amount of #{feature} objects"
       else
-                    "The #{section.name} must contain #{count < opposite_count ? "less" : "more" } #{style} objects than the #{section.opposite.name}"
+                    "The #{section.name} must contain #{count < opposite_count ? "less" : "more" } #{feature} objects than the #{section.opposite.name}"
       end
       rule.save
       rule
