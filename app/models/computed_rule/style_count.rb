11b76abe8328be8fa00d8a34198fcd426182f014
Shawn Isenhart <rughat@gmail.com>
2024-11-01 17:12:07 -0500

    Add StyleCount - counting style in a room
    This required a new method in room, as well as a way to get a single
    random room from section.  I also added a way to get the name of a room
    in lowercase format.

---
+++ app/models/computed_rule/style_count.rb
@@ -0,0 +1,23 @@
+module ComputedRule
+  class StyleCount < Requirement
+    def self.random_feature
+      Section.random_single.rooms.first
+    end
+
+    def self.build(house:, feature:)
+      room = house.get_room(feature)
+      count = room.count_different_styles
+      rule = create
+      rule.text = case count
+                  when 3
+                    "The #{room.lower_name} must have all three styles of objects"
+                  when 2
+                    "The #{room.lower_name} must have 2 different styles of objects"
+                  else
+                    "The #{room.lower_name} must have all objects the same style"
+                  end
+      rule.save
+      rule
+    end
+  end
+end
