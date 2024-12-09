431b875a663ba0d3bf97978b581e95e357787698
Shawn Isenhart <rughat@gmail.com>
2024-12-09 10:25:09 -0600

    Add comparable count of style vs color
    Compares in a large (two or four rooms) group the count of color vs the
    count of style in that group, and makes a requirement based on that
    count.

---
+++ spec/models/rules/comparable_count_of_color_vs_style_spec.rb
@@ -0,0 +1,53 @@
+require "rails_helper"
+
+RSpec.describe ComputedRule::ComparableCountOfColorVsStyle do
+  describe ".random_feature" do
+    it "calls and returns a random color and style" do
+      expect(Color).to receive(:random_color).and_return(Color.new("blue"))
+      expect(Style).to receive(:random).and_return("retro")
+      expect(described_class.random_feature.to_s).to eq("blue-retro")
+    end
+  end
+
+  describe ".build" do
+    let(:house) { instance_double(House) }
+    let(:sections) { class_double(Section) }
+    let(:color) { Color.new("blue") }
+    let(:style) { "retro" }
+    let(:feature) { "blue-retro" }
+    let(:selected_section) { instance_double(Section) }
+
+    it "randomly builds a rule from the given house when there is less of the style than of the color in the section" do
+      expect(sections).to receive(:random_multiroom).and_return(selected_section)
+      expect(selected_section).to receive(:name).and_return("top floor")
+      expect(Color).to receive(:new).with("blue").and_return(color)
+      expect(house).to receive(:count_colors).with(color: color, section: selected_section).and_return(4)
+      expect(house).to receive(:count_styles).with(style: style, section: selected_section).and_return(3)
+      subject = described_class.build(house: house, feature: feature, sections: sections)
+      expect(subject).to be_a(described_class)
+      expect(subject.text).to eq("The top floor must contain less retro objects than blue features (as objects and/or wall colors)")
+    end
+
+    it "randomly builds a rule from the given house when there is more of the style than of the color in the section" do
+      expect(sections).to receive(:random_multiroom).and_return(selected_section)
+      expect(selected_section).to receive(:name).and_return("top floor")
+      expect(Color).to receive(:new).with("blue").and_return(color)
+      expect(house).to receive(:count_colors).with(color: color, section: selected_section).and_return(2)
+      expect(house).to receive(:count_styles).with(style: style, section: selected_section).and_return(3)
+      subject = described_class.build(house: house, feature: feature, sections: sections)
+      expect(subject).to be_a(described_class)
+      expect(subject.text).to eq("The top floor must contain more retro objects than blue features (as objects and/or wall colors)")
+    end
+
+    it "randomly builds a rule from the given house when there is an equal amount of the style and of the color in the section" do
+      expect(sections).to receive(:random_multiroom).and_return(selected_section)
+      expect(selected_section).to receive(:name).and_return("top floor")
+      expect(Color).to receive(:new).with("blue").and_return(color)
+      expect(house).to receive(:count_colors).with(color: color, section: selected_section).and_return(3)
+      expect(house).to receive(:count_styles).with(style: style, section: selected_section).and_return(3)
+      subject = described_class.build(house: house, feature: feature, sections: sections)
+      expect(subject).to be_a(described_class)
+      expect(subject.text).to eq("The top floor must contain an equal number of retro objects and blue features (as objects and/or wall colors)")
+    end
+  end
+end
