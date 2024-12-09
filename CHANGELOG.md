33d446435ad53d38549ec1f5ea8f102c0ddf1ade
Shawn Isenhart <rughat@gmail.com>
2024-12-09 13:14:30 -0600

    Add comparable count of styles vs furnishings
    Also updated Changelog to include the Comparable group of rules as
    Unreleased

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - AspectMustBeMostFrequent rule
 - WallColorCount rule
+- ComparableCount rules
 
 ## [0.1.1] - 2024-11-26
 
---
+++ app/models/computed_rule/comparable_count_of_style_vs_furnishing.rb
@@ -0,0 +1,22 @@
+module ComputedRule
+  class ComparableCountOfStyleVsFurnishing < Requirement
+    def self.random_feature
+      "#{Style.random}-#{Furnishing.random.short_name}"
+    end
+
+    def self.build(house:, sections: Section, feature:)
+      section = sections.random_multiroom
+      style, furnishing = feature.split("-")
+      style_count = house.count_styles(style: style, section: section)
+      furnishing_count = house.count_furnishings(furnishing: furnishing, section: section)
+      rule = create
+      rule.text = if furnishing_count == style_count
+                    "The #{section.name} must contain an equal number of #{furnishing.pluralize} and #{style} features"
+      else
+        "The #{section.name} must contain #{(furnishing_count < style_count) ? "less" : "more"} #{furnishing.pluralize} than #{style} features"
+      end
+      rule.save
+      rule
+    end
+  end
+end
