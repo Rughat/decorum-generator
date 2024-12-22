810cbf885529447edb6b950dc89f9dd1be1165de
Shawn Isenhart <rughat@gmail.com>
2024-12-22 12:04:48 -0600

    Include a Style object that can be instantiated
    At the moment this isn't really being used, but having it allows future
    improvements when we can add icons to the style name

--- app/models/style.rb
+++ app/models/style.rb
@@ -1,6 +1,24 @@
 class Style
   STYLES = ["modern", "antique", "retro", "unusual"]
   def self.random(randomizer: Kernel)
-    STYLES[randomizer.rand(4)]
+    new(STYLES[randomizer.rand(4)])
   end
+
+  attr_reader :style
+
+  def initialize(style)
+    self.style = style
+  end
+
+  def ==(other)
+    style == other.style
+  end
+
+  def to_s
+    style
+  end
+
+  private
+
+  attr_writer :style
 end
--- spec/models/style_spec.rb
+++ spec/models/style_spec.rb
@@ -8,28 +8,28 @@ RSpec.describe Style do
     context "when random is 0" do
       it "randomly selects modern" do
         expect(randomizer).to receive(:rand).with(4).and_return(0)
-        expect(subject).to eq("modern")
+        expect(subject.to_s).to eq("modern")
       end
     end
 
     context "when random is 1" do
       it "randomly selects antique" do
         expect(randomizer).to receive(:rand).with(4).and_return(1)
-        expect(subject).to eq("antique")
+        expect(subject.style).to eq("antique")
       end
     end
 
     context "when random is 2" do
       it "randomly selects retro" do
         expect(randomizer).to receive(:rand).with(4).and_return(2)
-        expect(subject).to eq("retro")
+        expect(subject.to_s).to eq("retro")
       end
     end
 
     context "when random is 3" do
       it "randomly selects unusual" do
         expect(randomizer).to receive(:rand).with(4).and_return(3)
-        expect(subject).to eq("unusual")
+        expect(subject.style).to eq("unusual")
       end
     end
   end
