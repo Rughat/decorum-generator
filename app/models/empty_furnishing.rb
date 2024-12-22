b410f994af388c82d8c66e29274ee535b5464229
Shawn Isenhart <rughat@gmail.com>
2024-12-22 12:15:52 -0600

    Use EmptyMixin in EmptyFurnishing
    All of the methods in EmptyFurnishing are defined in the EmptyMixin, so
    we might as well use the mix in to make it so that if something gets
    changed, we have the updated code.

--- app/models/empty_furnishing.rb
+++ app/models/empty_furnishing.rb
@@ -1,29 +1,3 @@
 class EmptyFurnishing < Furnishing
-  def self.generate
-    EmptyFurnishing.create
-  end
-
-  def self.short_name
-    "empty space"
-  end
-
-  def self.empty?
-    true
-  end
-
-  def long_description
-    "An empty spot"
-  end
-
-  def color_obj
-    nil
-  end
-
-  def style
-    nil
-  end
-
-  def empty?
-    true
-  end
+  include EmptyMixin
 end
