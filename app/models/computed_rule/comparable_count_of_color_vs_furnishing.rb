6abf107649ba33f45fc262b3a7393f3edff554c0
Shawn Isenhart <rughat@gmail.com>
2024-12-23 07:35:58 -0600

    Add icons to color vs furnishing rule

--- app/models/computed_rule/comparable_count_of_color_vs_furnishing.rb
+++ app/models/computed_rule/comparable_count_of_color_vs_furnishing.rb
@@ -1,20 +1,21 @@
 module ComputedRule
   class ComparableCountOfColorVsFurnishing < Requirement
     def self.random_feature
-      "#{Color.random_color}-#{Furnishing.random.short_name}"
+      "#{Color.random_color}-#{Furnishing.random.to_s}"
     end
 
     def self.build(house:, sections: Section, feature:)
       section = sections.random_multiroom
-      c, furnishing = feature.split("-")
+      c, furnishing_raw = feature.split("-")
+      furnishing = furnishing_raw.constantize.new
       color = Color.new(c)
       color_count = house.count_colors(color: color, section: section)
-      furnishing_count = house.count_furnishings(furnishing: furnishing, section: section)
+      furnishing_count = house.count_furnishings(furnishing: furnishing.short_name, section: section)
       rule = create
       rule.text = if furnishing_count == color_count
-                    "The #{section.name} must contain an equal number of #{furnishing.pluralize} and #{color.display} features (as objects and/or wall colors)".html_safe
+                    "The #{section.name} must contain an equal number of #{furnishing.short_name.pluralize}#{furnishing.icon} and #{color.display} features (as objects and/or wall colors)".html_safe
       else
-        "The #{section.name} must contain #{(furnishing_count < color_count) ? "fewer" : "more"} #{furnishing.pluralize} than #{color.display} features (as objects and/or wall colors)".html_safe
+        "The #{section.name} must contain #{(furnishing_count < color_count) ? "fewer" : "more"} #{furnishing.short_name.pluralize}#{furnishing.icon} than #{color.display} features (as objects and/or wall colors)".html_safe
       end
       rule.save
       rule
--- spec/models/rules/comparable_count_of_color_vs_furnishing_spec.rb
+++ spec/models/rules/comparable_count_of_color_vs_furnishing_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe ComputedRule::ComparableCountOfColorVsFurnishing do
     it "calls and returns a random color and furnishing" do
       expect(Color).to receive(:random_color).and_return(Color.new("blue"))
       expect(Furnishing).to receive(:random).and_return(WallHanging)
-      expect(described_class.random_feature.to_s).to eq("blue-wall hanging")
+      expect(described_class.random_feature.to_s).to eq("blue-WallHanging")
     end
   end
 
@@ -13,41 +13,47 @@ RSpec.describe ComputedRule::ComparableCountOfColorVsFurnishing do
     let(:house) { instance_double(House) }
     let(:sections) { class_double(Section) }
     let(:color) { Color.new("blue") }
-    let(:furnishing) { "wall hanging" }
-    let(:feature) { "blue-wall hanging" }
+    let(:feature) { "blue-WallHanging" }
     let(:selected_section) { instance_double(Section) }
 
-    it "randomly builds a rule from the given house when there is fewer of the furnishing than of the color in the section" do
-      expect(sections).to receive(:random_multiroom).and_return(selected_section)
-      expect(selected_section).to receive(:name).and_return("top floor")
-      expect(Color).to receive(:new).with("blue").and_return(color)
-      expect(house).to receive(:count_colors).with(color: color, section: selected_section).and_return(4)
-      expect(house).to receive(:count_furnishings).with(furnishing: furnishing, section: selected_section).and_return(3)
-      subject = described_class.build(house: house, feature: feature, sections: sections)
-      expect(subject).to be_a(described_class)
-      expect(subject.text).to eq("The top floor must contain fewer wall hangings than <span class=\"blue\">blue<\/span> features (as objects and/or wall colors)")
-    end
+    context "when the feature is a wall hanging" do
+      let(:furnishing) { WallHanging.new }
 
-    it "randomly builds a rule from the given house when there is more of the furnishing than of the color in the section" do
-      expect(sections).to receive(:random_multiroom).and_return(selected_section)
-      expect(selected_section).to receive(:name).and_return("top floor")
-      expect(Color).to receive(:new).with("blue").and_return(color)
-      expect(house).to receive(:count_colors).with(color: color, section: selected_section).and_return(2)
-      expect(house).to receive(:count_furnishings).with(furnishing: furnishing, section: selected_section).and_return(3)
-      subject = described_class.build(house: house, feature: feature, sections: sections)
-      expect(subject).to be_a(described_class)
-      expect(subject.text).to eq("The top floor must contain more wall hangings than <span class=\"blue\">blue<\/span> features (as objects and/or wall colors)")
-    end
+      it "randomly builds a rule from the given house when there is fewer of the furnishing than of the color in the section" do
+        expect(WallHanging).to receive(:new).and_return(furnishing)
+        expect(sections).to receive(:random_multiroom).and_return(selected_section)
+        expect(selected_section).to receive(:name).and_return("top floor")
+        expect(Color).to receive(:new).with("blue").and_return(color)
+        expect(house).to receive(:count_colors).with(color: color, section: selected_section).and_return(4)
+        expect(house).to receive(:count_furnishings).with(furnishing: furnishing.short_name, section: selected_section).and_return(3)
+        subject = described_class.build(house: house, feature: feature, sections: sections)
+        expect(subject).to be_a(described_class)
+        expect(subject.text).to eq("The top floor must contain fewer wall hangings<span class=\"icon-wall-hanging\"></span> than <span class=\"blue\">blue<\/span> features (as objects and/or wall colors)")
+      end
 
-    it "randomly builds a rule from the given house when there is an equal amount of the furnishing and of the color in the section" do
-      expect(sections).to receive(:random_multiroom).and_return(selected_section)
-      expect(selected_section).to receive(:name).and_return("top floor")
-      expect(Color).to receive(:new).with("blue").and_return(color)
-      expect(house).to receive(:count_colors).with(color: color, section: selected_section).and_return(3)
-      expect(house).to receive(:count_furnishings).with(furnishing: furnishing, section: selected_section).and_return(3)
-      subject = described_class.build(house: house, feature: feature, sections: sections)
-      expect(subject).to be_a(described_class)
-      expect(subject.text).to eq("The top floor must contain an equal number of wall hangings and <span class=\"blue\">blue<\/span> features (as objects and/or wall colors)")
+      it "randomly builds a rule from the given house when there is more of the furnishing than of the color in the section" do
+        expect(WallHanging).to receive(:new).and_return(furnishing)
+        expect(sections).to receive(:random_multiroom).and_return(selected_section)
+        expect(selected_section).to receive(:name).and_return("top floor")
+        expect(Color).to receive(:new).with("blue").and_return(color)
+        expect(house).to receive(:count_colors).with(color: color, section: selected_section).and_return(2)
+        expect(house).to receive(:count_furnishings).with(furnishing: furnishing.short_name, section: selected_section).and_return(3)
+        subject = described_class.build(house: house, feature: feature, sections: sections)
+        expect(subject).to be_a(described_class)
+        expect(subject.text).to eq("The top floor must contain more wall hangings<span class=\"icon-wall-hanging\"></span> than <span class=\"blue\">blue<\/span> features (as objects and/or wall colors)")
+      end
+
+      it "randomly builds a rule from the given house when there is an equal amount of the furnishing and of the color in the section" do
+        expect(WallHanging).to receive(:new).and_return(furnishing)
+        expect(sections).to receive(:random_multiroom).and_return(selected_section)
+        expect(selected_section).to receive(:name).and_return("top floor")
+        expect(Color).to receive(:new).with("blue").and_return(color)
+        expect(house).to receive(:count_colors).with(color: color, section: selected_section).and_return(3)
+        expect(house).to receive(:count_furnishings).with(furnishing: furnishing.short_name, section: selected_section).and_return(3)
+        subject = described_class.build(house: house, feature: feature, sections: sections)
+        expect(subject).to be_a(described_class)
+        expect(subject.text).to eq("The top floor must contain an equal number of wall hangings<span class=\"icon-wall-hanging\"></span> and <span class=\"blue\">blue<\/span> features (as objects and/or wall colors)")
+      end
     end
   end
 end
