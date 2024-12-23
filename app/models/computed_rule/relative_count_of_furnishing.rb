44abfc68c18251e081a4296ae3876713085ce40e
Shawn Isenhart <rughat@gmail.com>
2024-12-23 06:52:13 -0600

    Change relative count of furnishing to use icons

--- app/models/computed_rule/relative_count_of_furnishing.rb
+++ app/models/computed_rule/relative_count_of_furnishing.rb
@@ -1,18 +1,19 @@
 module ComputedRule
   class RelativeCountOfFurnishing < Requirement
     def self.random_feature
-      Furnishing.random.short_name
+      Furnishing.random.to_s
     end
 
     def self.build(house:, sections: Section, feature:)
       section = sections.random_multiroom_opposable
-      count = house.count_furnishings(furnishing: feature, section: section)
-      opposite_count = house.count_furnishings(furnishing: feature, section: section.opposite)
+      furnishing = feature.constantize.new
+      count = house.count_furnishings(furnishing: furnishing.short_name, section: section)
+      opposite_count = house.count_furnishings(furnishing: furnishing.short_name, section: section.opposite)
       rule = create
       rule.text = if count == opposite_count
-        "The #{section.name} must contain an equal amount of #{feature.pluralize} as the #{section.opposite.name}"
+        "The #{section.name} must contain an equal amount of #{furnishing.short_name.pluralize}#{furnishing.icon} as the #{section.opposite.name}".html_safe
       else
-        "The #{section.name} must contain #{(count < opposite_count) ? "fewer" : "more"} #{feature.pluralize} than the #{section.opposite.name}"
+        "The #{section.name} must contain #{(count < opposite_count) ? "fewer" : "more"} #{furnishing.short_name.pluralize}#{furnishing.icon} than the #{section.opposite.name}".html_safe
       end
       rule.save
       rule
