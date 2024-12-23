f3ff25dafd094e36a2212064825793cb139e2af2
Shawn Isenhart <rughat@gmail.com>
2024-12-23 09:41:26 -0600

    Add furnishing icons to Comp Style vs Furnishing rule

--- app/models/computed_rule/comparable_count_of_style_vs_furnishing.rb
+++ app/models/computed_rule/comparable_count_of_style_vs_furnishing.rb
@@ -1,19 +1,20 @@
 module ComputedRule
   class ComparableCountOfStyleVsFurnishing < Requirement
     def self.random_feature
-      "#{Style.random}-#{Furnishing.random.short_name}"
+      "#{Style.random}-#{Furnishing.random.to_s}"
     end
 
     def self.build(house:, sections: Section, feature:)
       section = sections.random_multiroom
-      style, furnishing = feature.split("-")
+      style, furnishing_raw = feature.split("-")
+      furnishing = furnishing_raw.constantize.new
       style_count = house.count_styles(style: style, section: section)
-      furnishing_count = house.count_furnishings(furnishing: furnishing, section: section)
+      furnishing_count = house.count_furnishings(furnishing: furnishing.short_name, section: section)
       rule = create
       rule.text = if furnishing_count == style_count
-                    "The #{section.name} must contain an equal number of #{furnishing.pluralize} and #{style} features"
+                    "The #{section.name} must contain an equal number of #{furnishing.short_name.pluralize}#{furnishing.icon} and #{style} features"
       else
-        "The #{section.name} must contain #{(furnishing_count < style_count) ? "fewer" : "more"} #{furnishing.pluralize} than #{style} features"
+        "The #{section.name} must contain #{(furnishing_count < style_count) ? "fewer" : "more"} #{furnishing.short_name.pluralize}#{furnishing.icon} than #{style} features"
       end
       rule.save
       rule
